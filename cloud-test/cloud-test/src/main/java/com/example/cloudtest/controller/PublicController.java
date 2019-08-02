d79a1de25a6424551f618e317c7d2858410009dc
Grace_Cheah <grace_cheah@dell.com>
2019-08-02 10:50:04 +0800

    ww

--- cloud-test/cloud-test/src/main/java/com/example/cloudtest/controller/PublicController.java
+++ cloud-test/cloud-test/src/main/java/com/example/cloudtest/controller/PublicController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicController {
   @GetMapping(value = "/")
   public String test() {
-    return "{\"message\": \"test\"}";
+    return "{\"message\": \"testss\"}";
   }
 }
